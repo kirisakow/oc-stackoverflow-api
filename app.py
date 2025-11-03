52ddac57ec85c0b1af44f4eb5880f4ffe404da81
Kiril Isakov <kirix.isakov@gmail.com>
2025-11-03 20:31:53 +0100

    API de prédiction fonctionne avec les pickles et données sauvegardés en local

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
 from fastapi import FastAPI, Request
 from typing import Any
 import argparse
+import pickle
 import uvicorn
 
-app = FastAPI(description="L'API HTTP pour mon projet OpenClassrooms «Étiquetage des questions Stackoverflow»", )
+app = FastAPI(description="L'API HTTP pour mon projet OpenClassrooms «Prédiction de l'étiquetage des questions Stackoverflow»", )
 
 
 @app.post("/predict")
 async def predict(req: Request) -> Any:
-    data = await req.json()
-    print(f'input: {data}')
-    data['foo'] *= 10
-    print(f'output: {data}')
-    return data
+    req_data = (await req.body()).decode()
+    result = model.predict(
+        vectorizer.transform([req_data])[:, :944]
+    ) # result = array([[0, 1, 1, 0, ...]])
+    result = mlb.inverse_transform(result)[0] # result = ('tag1', 'tag2', 'tag3', ...)
+    return result
 
 
 if __name__ == "__main__":
+    with    open('models/model.pkl', 'rb') as fmodel, \
+            open('models/vectorizer_filtered.pkl', 'rb') as fvec, \
+            open('models/mlb.pkl', 'rb') as fmlb:
+        model = pickle.load(fmodel)
+        vectorizer = pickle.load(fvec)
+        mlb = pickle.load(fmlb)
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", required=True,
