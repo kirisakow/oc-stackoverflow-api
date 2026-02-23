4616ab9fb96f9055858aba1f1ce7aaf8e00cc776
Kiril Isakov <kirix.isakov@gmail.com>
2026-02-23 20:28:04 +0100

    code : avancement et renommage des fichiers

---
+++ api_server.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, Request
+from typing import Any
+import argparse
+import joblib
+import uvicorn
+from typing import List
+
+
+def prediction(req_data: str) -> List[str]:
+    result = model.predict(
+        # vectorizer.transform([req_data])[:, :944] -- 2025, the hack
+        vectorizer.transform([req_data])
+    ) # result = array([[0, 1, 1, 0, ...]])
+    result = mlb.inverse_transform(result)[0] # result = ('tag1', 'tag2', 'tag3', ...)
+    return result
+
+
+app = FastAPI(description="L'API HTTP pour mon projet OpenClassrooms «Prédiction de l'étiquetage des questions Stackoverflow»", )
+
+
+@app.post("/predict")
+async def predict(req: Request) -> Any:
+    req_data = (await req.body()).decode()
+    return prediction(req_data)
+
+
+if __name__ == "__main__":
+    with open('best_estimator_TfidfVectorizer_20000_LogisticRegression.joblib', 'rb') as fmodel, \
+         open('vectorizer_TfidfVectorizer_100000.pkl', 'rb') as fvec, \
+         open('mlb_100000.pkl', 'rb') as fmlb:
+        model = joblib.load(fmodel)
+        vectorizer = joblib.load(fvec)
+        mlb = joblib.load(fmlb)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", required=True,
+                        help="Port number to run the server on")
+    args = parser.parse_args()
+    port = int(args.port)
+
+    try:
+        uvicorn.run(app, host="127.0.0.1", port=port)
+    except KeyboardInterrupt:
+        pass
