3a7b3b0e33908410c0cda548f96348ca106f710b
Kiril Isakov <kirix.isakov@gmail.com>
2026-03-17 15:53:13 +0100

    unit testing : supprimer l'URL du serveur Azure pour éviter l'effet trompeur, le but de TestClient de Starlette étant d'opérer en local. Voir la doc FastAPI : - https://fastapi.tiangolo.com/tutorial/testing/ - https://fastapi.tiangolo.com/reference/testclient/

--- test_main.py
+++ test_main.py
@@ -3,18 +3,17 @@ from fastapi.testclient import TestClient
 from main import app, prediction
 
 client = TestClient(app)
-url_public = 'https://oc-stackoverflow-api-1-cugua9avgebahha4.francecentral-01.azurewebsites.net'
 
 
 def test_home():
-    response = client.get(f'{url_public}/')
+    response = client.get('/')
     assert response.status_code == 200
     assert response.json() == "Hello, World!"
 
 
 def test_prediction_endpoint():
     test_data = "How to perform unit testing in Python?"
-    response = client.post(f'{url_public}/predict', content=test_data)
+    response = client.post('/predict', content=test_data)
     assert response.status_code == 200
     assert any([
         isinstance(response.json(), list),
